761cd7d1d7dad18c5b5cdaebca6da655f7d66f27
Jonas_Jones <91549607+J-onasJones@users.noreply.github.com>
2023-02-01 21:47:35 +0100

    some inbetween progress

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express();
+app.get('/', (req, res) => {
+    res.send('Welcome to the jonas_jones-api!')
+});
+
+app.get('/projects', (req, res) => {
+    const keyword = req.query.keyword;
+    const version = req.query.version;
+    const platform = req.query.platform;
+    const topic = req.query.topic;
+    return res.status(200).json({
+        success: true,
+        projects: {
+            keyword: keyword,
+            version: version,
+            platform: platform,
+            topic: topic,
+        }
+    })
+    
+
+});
+app.listen(8000, () => {
+    console.log('api listening on port 8000!')
+});
