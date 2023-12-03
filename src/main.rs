6abba646c76cffb5fcff0cc60e5b2035e935554b
Jonas_Jones <91549607+J-onasJones@users.noreply.github.com>
2023-12-03 18:23:36 +0100

    proof of concept

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use warp::Filter;
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    // GET /hello/warp => 200 OK with body "Hello, warp!"
+    let hello = warp::path!("hello" / String)
+        .map(|name| format!("Hello, {}!", name));
+
+    let root = warp::path::end()
+        .map(|| "Hello, World!");
+
+
+    let routes = hello.or(root);
+
+    warp::serve(routes)
+        .run(([127, 0, 0, 1], 3030))
+        .await;
+}
