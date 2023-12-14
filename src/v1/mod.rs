693dd6638c7436d8c49f86266a6bc5374e68a76e
J-onasJones <me@jonasjones.dev>
2023-12-15 00:19:25 +0100

    added /debug/headers

--- src/v1/mod.rs
+++ src/v1/mod.rs
@@ -1,8 +1,10 @@
 mod builtin;
+mod debug;
 mod kcomebacks;
 mod updates;
 
 pub use builtin::get_builtin_routes as get_v1_builtin_routes;
+pub use debug::get_debug_routes as get_v1_debug_routes;
 pub use kcomebacks::get_kcomebacks_routes as get_v1_kcomebacks_routes;
 pub use updates::get_updates_routes as get_v1_updates_routes;
 
@@ -10,6 +12,7 @@ use warp::Filter;
 
 pub fn get_v1_routes() -> impl warp::Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
     return get_v1_builtin_routes()
+        .or(get_v1_debug_routes())
         .or(get_v1_kcomebacks_routes())
         .or(get_v1_updates_routes());
 }
--- src/v1/updates/minecraft/mods/mod.rs
+++ src/v1/updates/minecraft/mods/mod.rs
@@ -15,3 +15,15 @@ pub fn get_mods_paths() -> impl warp::Filter<Extract = impl warp::Reply, Error =
 fn handle_path(modname: String, loadername: String, version: String, remote_ip: Option<std::net::SocketAddr>) -> String {
     format!("modname: {}, loadername: {}, version: {}, IP: {}", modname, loadername, version, remote_ip.unwrap_or(std::net::SocketAddr::from(([0, 0, 0, 0], 0))).ip())
 }
+
+fn handle_with_headers(
+    headers: warp::http::HeaderMap,
+) -> String {
+    // Iterate through the headers and print them
+    for (name, value) in headers.iter() {
+        println!("Header: {}: {}", name, value.to_str().unwrap_or("Invalid UTF-8"));
+    }
+
+    // Respond with a message or perform other actions as needed
+    "Headers received".to_string()
+}
