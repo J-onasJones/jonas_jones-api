5aefb1a7c79364cb1a9cd4ffee44813bbe7a9faf
J-onasJones <me@jonasjones.dev>
2024-01-03 00:49:29 +0100

    Added /v1/projects api branch

--- src/v1/mod.rs
+++ src/v1/mod.rs
@@ -1,11 +1,13 @@
 mod builtin;
 mod debug;
 mod kcomebacks;
+mod projects;
 mod updates;
 
 pub use builtin::get_builtin_routes as get_v1_builtin_routes;
 pub use debug::get_debug_routes as get_v1_debug_routes;
 pub use kcomebacks::get_kcomebacks_routes as get_v1_kcomebacks_routes;
+pub use projects::get_project_routes as get_v1_project_routes;
 pub use updates::get_updates_routes as get_v1_updates_routes;
 
 use warp::Filter;
@@ -14,5 +16,6 @@ pub fn get_v1_routes() -> impl warp::Filter<Extract = impl warp::Reply, Error = 
     return get_v1_builtin_routes()
         .or(get_v1_debug_routes())
         .or(get_v1_kcomebacks_routes())
+        .or(get_v1_project_routes())
         .or(get_v1_updates_routes());
 }
--- src/v1/projects/mod.rs
+++ src/v1/projects/mod.rs
@@ -1,17 +1,26 @@
+use serde::{Deserialize, Serialize};
+use serde_json::{Value, json};
+use warp::Filter;
+use reqwest::Error;
+use std::collections::HashMap;
 
+mod filter;
 
+use filter::get_project_filter_routes;
+
+use crate::error_responses::InternalServerError;
 
 pub fn get_project_routes() -> impl warp::Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
     warp::path("v1").and(warp::path("projects"))
 
         .and(warp::path("last_update").and(warp::get()).and_then(last_update)
         .or(warp::path("start_update").map(|| "Not implemented yet"))
-        .or(get_kcomebacks_upcoming_routes()))
+        .or(get_project_filter_routes()))
 }
 
 // get json data from https://https://cdn.jonasjones.dev/api/projects/projects.json
 pub async fn fetch_data() -> Result<serde_json::Value, Error> {
-    let url = "https://https://cdn.jonasjones.dev/api/projects/projects.json";
+    let url = "https://cdn.jonasjones.dev/api/projects/projects.json";
     let response = reqwest::get(url).await?;
 
     if response.status().is_success() {
@@ -34,4 +43,41 @@ async fn last_update() -> Result<impl warp::Reply, warp::Rejection> {
         serde_json::Value::String(last_update) => Ok(warp::reply::json(&last_update)),
         _ => Err(warp::reject::custom(InternalServerError)),
     }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Project {
+    pub title: String,
+    pub description: String,
+    pub status: String,
+    pub statuscolor: String,
+    pub categories: Vec<String>,
+    pub languages: HashMap<String, i32>,
+    pub gh_api: String,
+    pub version: String,
+    pub backgroud: String,
+    pub links: HashMap<String, String>,
+    pub visible: bool,
+    pub last_update: i64,
+}
+
+pub fn create_json_response(items: Vec<&Project>, total_results: usize) -> Value {
+    // Serialize the vector of items to a JSON array
+    let results_array: Vec<Value> = items.into_iter().map(|item| json!(item)).collect();
+
+    // Build the final JSON object with "results" and "total_results" fields
+    let json_response = json!({
+        "results": results_array,
+        "total_results": total_results,
+    });
+
+    json_response
+}
+
+pub fn parse_item(item: &Value) -> Project {
+    // Parse the item into a struct
+    let item: Project = serde_json::from_value(item.clone()).unwrap();
+
+    // Return the parsed item
+    item
 }
