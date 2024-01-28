f201bc7fda0d564102214b7d2c7de798e1914ad3
Jonas_Jones <91549607+JonasunderscoreJones@users.noreply.github.com>
2024-01-28 03:10:41 +0100

    Added new branch /v1/update

--- src/v1/mod.rs
+++ src/v1/mod.rs
@@ -2,17 +2,17 @@ mod builtin;
 mod debug;
 mod kcomebacks;
 mod projects;
-mod updates;
+mod update;
 
 pub use builtin::get_builtin_routes as get_v1_builtin_routes;
 pub use debug::get_debug_routes as get_v1_debug_routes;
 pub use kcomebacks::get_kcomebacks_routes as get_v1_kcomebacks_routes;
 pub use projects::get_project_routes as get_v1_project_routes;
-pub use updates::get_updates_routes as get_v1_updates_routes;
+pub use update::get_update_routes as get_v1_updates_routes;
 
 use warp::Filter;
 
-pub fn get_v1_routes() -> impl warp::Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+pub fn get_v1_routes() -> impl warp::Filter<Extract = impl warp::Reply + warp::generic::Tuple, Error = warp::Rejection> + Clone {
     return get_v1_builtin_routes()
         .or(get_v1_debug_routes())
         .or(get_v1_kcomebacks_routes())
